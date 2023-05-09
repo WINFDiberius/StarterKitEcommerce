31964ba4daeb3e31baaa152c5c811b9b7b9d2be0
dsattler <01010174@edu.campus02.at>
2023-05-09 18:54:40 +0200

    Initial commit

---
+++ src/org/campus02/ecom/DataFileException.java
@@ -0,0 +1,19 @@
+package org.campus02.ecom;
+
+public class DataFileException extends Exception{
+    public DataFileException() {
+    }
+
+    public DataFileException(String message) {
+        super(message);
+    }
+
+    public DataFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataFileException(Throwable cause) {
+        super(cause);
+    }
+    
+}
